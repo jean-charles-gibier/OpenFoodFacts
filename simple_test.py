1363dfd62012d282eaeb5a652059f8cea075e388
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-05-20 13:29:13 +0200

    Push tortoise
    Signed-off-by: Jean-Charles Gibier <jeancharlesg@free.fr>

---
+++ simple_test.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import json
+
+
+# ajouter l'appel via urllib
+
+
+with open('D:/github/OCR/garbage/categories2.json', encoding="utf8") as json_file:
+    data = json.load(json_file)
+    nb_cat = 0
+    for p in data['tags']:
+        if ((p['id']).startswith('fr:')  ):
+            print('Name: ' + p['name'])
+            print('Website: ' + p['url'])
+            print('Id: ' + p['id'])
+            print('')
+            nb_cat = nb_cat + 1
+    print('Nb categorie :' + str(nb_cat))
+
+    # https://world.openfoodfacts.org/cgi/search.pl?action=process & tagtype_0=categories&tag_contains_0=contains&tag_0=Saucissons%20secs
+    # avec ça on peut faire une table produit + catégorie
