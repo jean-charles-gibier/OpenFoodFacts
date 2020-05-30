49985e633f341a48cbaa9b80d8d068eb1c03d126
jean-charles-gibier <jeancharlesg@free.fr>
2020-05-30 19:35:30 +0200

    flake session #11

--- core/model/category.py
+++ core/model/category.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*- #
+"""
+category definition
+"""
 
 
 class Category:
-    """Classe permettant de créer une category """
-
     """ category constructor """
-
     def __init__(self, **category):
         # petite verrue l'attribut id de off est renommé en id_off
         self._columns_values = dict()
@@ -17,32 +17,41 @@ class Category:
     # builder json
     @classmethod
     def buildfromjson(cls, **category):
+        """ data from json"""
         return cls(**category)
 
     # builder mysql
     @classmethod
     def buildfrommysql(cls, **category):
+        """ data from mysql"""
         return cls(**category)
 
     def __str__(self):
+        """ express yourself"""
         return str(self._columns_values)
 
     @property
-    def id(self):
+    def ident(self):
+        """ should return id """
         return None
 
     @property
     def id_off(self):
+        """ returns openfactsfood id """
         return self._columns_values['id_off']
 
     @property
     def name(self):
+        """ returns off name """
         return self._columns_values['name']
 
     @property
     def columns_names(self):
+        """ returns dict of names
+        (each keys is the name of the element)"""
         return self._columns_names
 
     @property
     def columns_values(self):
+        """ returns dict of values """
         return self._columns_values
--- core/model/product.py
+++ core/model/product.py
@@ -1,75 +1,109 @@
 # -*- coding: utf-8 -*- #
+"""
+product definition
+"""
 
 
 class Product():
-    """ Product object """
-    """ constructor """
+    """ object constructor """
 
     def __init__(self, **product):
         self._columns_values = dict()
-        self._columns_values['id'] = product['id'] if 'id' in product else None
-        self._columns_values['ean_code'] = product['code'] if 'code' in product else None
-        self._columns_values['product_name'] = product['product_name'] if 'product_name' in product else None
-        self._columns_values['generic_name'] = product['generic_name_fr'] if 'generic_name_fr' in product else None
-        self._columns_values['brands'] = product['brands'] if 'brands' in product else None
-        self._columns_values['stores'] = product['stores'] if 'stores' in product else None
-        self._columns_values['url'] = product['url'] if 'url' in product else None
+        self._columns_values['id'] = product['id'] \
+            if 'id' in product else None
+        self._columns_values['ean_code'] = product['code'] \
+            if 'code' in product else None
+        self._columns_values['product_name'] = product['product_name'] \
+            if 'product_name' in product else None
+        self._columns_values['generic_name'] = product['generic_name_fr'] \
+            if 'generic_name_fr' in product else None
+        self._columns_values['brands'] = product['brands'] \
+            if 'brands' in product else None
+        self._columns_values['stores'] = product['stores'] \
+            if 'stores' in product else None
+        self._columns_values['url'] = product['url'] \
+            if 'url' in product else None
         self._columns_values['nutrition_grade'] = product[
-            'nutrition_grade_fr'] if 'nutrition_grade_fr' in product else None
-        self._columns_names = ['ean_code', 'product_name', 'generic_name', 'brands', 'stores', 'url', 'nutrition_grade']
+            'nutrition_grade_fr'] \
+            if 'nutrition_grade_fr' in product else None
+        self._columns_names = \
+            ['ean_code', 'product_name', 'generic_name',
+             'brands', 'stores', 'url', 'nutrition_grade']
 
     # builder json
     @classmethod
     def buildfromjson(cls, **product):
+        """ data from product """
         return cls(**product)
 
     # builder mysql
     @classmethod
     def buildfrommysql(cls, **product):
+        """ data from mysql """
         # get 1 map from 2 with different origins => fusion with key change
-        translation = {'ean_code': 'code', 'generic_name': 'generic_name_fr', 'nutrition_grade': 'nutrition_grade_fr'}
-        new_map = dict([((k in translation and (translation.get(k))) or k, v) for k, v in product.items()])
+        translation = {
+            'ean_code': 'code',
+            'generic_name': 'generic_name_fr',
+            'nutrition_grade': 'nutrition_grade_fr'}
+        new_map = dict([
+            ((k in translation and (translation.get(k))) or k, v)
+            for k, v in product.items()])
         return cls(**new_map)
 
     def __str__(self):
         return str(self._columns_values)
 
     @property
-    def id(self):
+    def ident(self):
+        """ returns id product """
         return self._columns_values['id']
 
     @property
     def ean_code(self):
+        """ returns barcode  """
         return self._columns_values['ean_code']
 
     @property
     def name(self):
+        """ returns name """
         return self._columns_values['name']
 
     @property
     def generic_name(self):
+        """ returns generic_name
+        property openfactsfood """
         return self._columns_values['generic_name']
 
     @property
     def brands(self):
+        """ returns brand names
+        property openfactsfood """
         return self._columns_values['brands']
 
     @property
     def stores(self):
+        """ returns store names
+        property openfactsfood """
         return self._columns_values['stores']
 
     @property
     def url(self):
+        """ returns url access
+        property openfactsfood """
         return self._columns_values['url']
 
     @property
     def nutrition_grade(self):
+        """ returns nutrition grade
+        property openfactsfood """
         return self._columns_values['nutrition_grade']
 
     @property
     def columns_names(self):
-        return self._columns_values
+        """ returns dict of names """
+        return self._columns_names
 
     @property
     def columns_values(self):
+        """ returns dict of values """
         return self._columns_values
